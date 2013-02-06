15aa53a7c95e4e4418fea05f43f14d82815aa87a
Umair <omerjerk@gmail.com>
2013-02-06 21:23:43 +0530

    social network icon bug fix
    and removed the styles from the main index file

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
 if(mysql_connect("localhost","root","") && mysql_select_db("nyp"))
 {
@@ -39,9 +39,12 @@ else
 
 <html>
 <head>
-  <link rel="stylesheet" type="text/css" href="styles/layout.css" />
-  <script src="js/jquery-1.9.0.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="styles/layout.css"/>
 <script>
+function resize()
+{
+	
+}
 function loadwebsite()
 {
 	document.getElementById('indexopen').style.display="none";
@@ -89,10 +92,17 @@ function mouseCoord(e)
 </script>
 </head>
 
-<body>
+<body onresize="resize();">
 <div id="staticlinks">
+</div><div id="staticlinks">
+<a class="thumb" href="https://www.facebook.com/" target="_blank"><img src="images/48/facebook.png"><span><img src="images/60/facebook.png" alt=""></span></a>
+<a class="thumb" href="https://www.twitter.com/" target="_blank"><img src="images/48/twitter.png"><span><img src="images/60/twitter.png" alt=""></span></a>
+<a class="thumb" href="https://www.youtube.com/" target="_blank"><img src="images/48/youtube.png"><span><img src="images/60/youtube.png" alt=""></span></a>
+<a class="thumb" href="https://www.youtube.com/" target="_blank"><img src="images/48/feed.png"><span><img src="images/60/feed.png" alt=""></span></a>
+<a class="thumb" href="https://www.google.com/" target="_blank"><img src="images/48/google.png"><span><img src="images/60/google.png" alt=""></span></a>
 
 </div>
+
 <div id="indexopen" >
 	<div id="imageopen">
 		<div id="regupdatesdiv">
@@ -117,37 +127,81 @@ function mouseCoord(e)
 		header div
 		</div>
 		<div id="imageheaderdiv"> 
-		imageheader
+		<img src="Party_Header.png">
 		</div>
 		<div id="navmenudiv" style="z-index:10">
 		<ul class="navmenu">
 			<li><a href="#">Home</a></li>
 			<li><a href="#">About</a></li>
-			<li><a href="#">OurLeaders</a>
+			<li><a href="#">Our Agenda&nbsp;&#8595;</a>
 				<ul class="submenu">
-					<li><a href="#">our leader1</a></li>
-					<li><a href="#">our leader2</a></li>
-					<li><a href="#">our leader3</a></li>
-					<li><a href="#">our leader4</a></li>
+					<li style="padding-top:5px;"><a href="#">General</a></li>
+					<li><a href="#">Youth</a></li>
+					<li><a href="#">Deprived Section</a></li>
+					<li><a href="#">Common Man</a></li>
 				</ul>
 			</li>
-			<li><a href="#">Organistion</a>
+			<li><a href="#">Organistion&nbsp;&#8595;</a>
 				<ul class="submenu">
+					<li ><a href="#">News and Stories</a></li>
 					<li><a href="#">News and Stories</a></li>
 					<li><a href="#">News and Stories</a></li>
-					<li><a href="#">News and Stories</a></li>
-					<li><a href="#">News and Stories</a></li>
+					<li><a href="#">News</a></li>
 				</ul>
 			</li>
-			<li><a href="#">States</a>
+			<li><a href="#">Centres&nbsp;&#8595;</a>
 				<ul class="submenu">
-					<li><a href="#">News and Stories</a></li>
-					<li><a href="#">News and Stories</a></li>
-					<li><a href="#">News and Stories</a></li>
-					<li><a href="#">News and Stories</a></li>
+					<li ><a href="#">North India</a>
+						<ul class="subsubmenu">
+							<li><a href="#">Jammu & Kashmir</a></li>
+							<li><a href="#">Himachal Pradesh</a></li>
+							<li><a href="#">Uttarakhand</a></li>
+							<li><a href="#">Punjab</a></li>
+							<li><a href="#">Haryana</a></li>
+							<li><a href="#">Delhi</a></li>
+							<li><a href="#">Uttar Pradesh</a></li>
+							<li><a href="#">Bihar</a></li>
+						</ul>
+					</li>
+					<li><a href="#">West</a>
+						<ul class="subsubmenu">
+							<li><a href="#">Rajasthan</a></li>
+							<li><a href="#">Gujarat</a></li>
+						</ul>
+					</li>
+					<li><a href="#">East</a>
+						<ul class="subsubmenu">
+							<li><a href="#">Sikkim</a></li>
+							<li><a href="#">Assam</a></li>
+							<li><a href="#">West Bengal</a></li>
+							<li><a href="#">Mizoram</a></li>
+							<li><a href="#">Meghalaya</a></li>
+							<li><a href="#">Arunachal Pradesh</a></li>
+							<li><a href="#">Tripura</a></li>
+							<li><a href="#">Nagaland</a></li>
+							<li><a href="#">Manipal</a></li>
+						</ul>
+					</li>
+					<li><a href="#">South</a>
+						<ul class="subsubmenu">
+							<li><a href="#">Maharashtra</a></li>
+							<li><a href="#">Karnataka</a></li>
+							<li><a href="#">Goa</a></li>
+							<li><a href="#">Orrisa</a></li>
+							<li><a href="#">Andhra Pradesh</a></li>
+							<li><a href="#">Kerela</a></li>
+							<li><a href="#">Tamil Nadu</a></li>
+						</ul></li>
+					<li><a href="#">Central</a>
+						<ul class="subsubmenu">
+							<li><a href="#">Madhya Pradesh</a></li>
+							<li><a href="#">Jharkhand</a></li>
+							<li><a href="#">Chattisgarh</a></li>
+						</ul>
+					</li>
 				</ul>
 			</li>
-			<li><a href="#">News and Stories</a></li>
+			<li><a href="#">News</a></li>
 			<li><a href="#">Gallery</a></li>
 			<li><a href="#">Contact Us</a></li>
 		</ul>
@@ -160,7 +214,39 @@ function mouseCoord(e)
 		joinform
 		</div>
 		<div id="footerdiv">
-		footer
+			<div >
+				<span style="color:#FFFFCC;text-align:center;position:relative;width:99%;display:block;">AGENDA</span>
+				<ul>
+					<li>Jan lokpal</li>
+					<li>Right to reject</li>
+					<li>Right to recall</li>
+					<li>Political decentralization</li>
+					<li>Rising Prices</li>
+					<li>Others</li>
+				</ul>
+			</div>
+			<div>
+				<span style="color:#FFFFCC;text-align:center;position:relative;width:99%;display:block;">OFFFICE BEARERS</span>
+				<ul>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+				</ul>
+			</div>
+			<div>
+				<span style="color:#FFFFCC;text-align:center;position:relative;width:99%;display:block;">AGENDA</span>
+				<ul>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+					<li>Arvind Kejriwal</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </div>
