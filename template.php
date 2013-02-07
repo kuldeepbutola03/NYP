4d50ff9b43151710badc8b68d10807926096ccdf
abhinavdahiya <abhinavdtu2012@gmail.com>
2013-02-07 09:42:06 +0530

    template for further pages

--- template.php
+++ template.php
@@ -1,80 +1,9 @@
-<?php
-
-if(mysql_connect("localhost","root","") && mysql_select_db("nyp"))
-{
-	if(isset($_POST['emailregupdates']))
-	{
-		if(!empty($_POST['emailregupdates']))
-		{
-			$email=$_POST['emailregupdates'];
-			
-			$query = "SELECT id FROM regupdates WHERE email='".$email."'";
-			if($result=mysql_query($query))
-			{
-				
-				$num_rows = mysql_num_rows($result);
-				if($num_rows==0)
-				{
-					$query = "INSERT INTO regupdates VALUES(NULL,'".$email."')";
-					mysql_query($query);
-				}
-				else if($num_rows>=1)
-				{
-					echo "already registered";
-				}
-			}
-			else
-			{
-				echo mysql_error();
-			}
-		}
-	}
-	
-}
-else
-{
-	echo mysql_error();
-}
-?>
 
 <html>
 <head>
 <title>National Youth Party</title>
-<link rel="stylesheet" type="text/css" href="./css/indexlayout.css" />
-<style>
-
-</style>
+<link rel="stylesheet" type="text/css" href="./css/template.css" />
 <script>
-function resize()
-{
-	
-}
-function loadwebsite()
-{
-	document.getElementById('indexopen').style.display="none";
-	document.getElementById('indexwebsite').style.display="block";
-	document.getElementById('indexwebsite').style.opacity="1";
-	
-}
-function regexcheckemail()
-{
-	email = document.getElementById('emailregupdates').value;
-	var regex = /^[a-zA-Z0-9._-]+@[a-zA-Z]+.[a-zA-Z.]+/;
-	if(email.match(regex))
-	{
-		document.getElementById('emailregupdateshelp').style.display="none";
-		document.getElementById('regupdatesdiv').style.height="12%";
-		return true;
-		
-	}
-	else
-	{
-		document.getElementById('emailregupdateshelp').style.display="block";
-		document.getElementById('regupdatesdiv').style.height="15%";
-		return false;
-	}
-}
-
 document.captureEvents(Event.MOUSEMOVE);
 document.onmousemove=mouseCoord;
 
@@ -108,26 +37,7 @@ function mouseCoord(e)
 <a class="thumb" href="https://www.google.com/" target="_blank"><img src="./images/google.png"><span><img src="icon\60 X 60\google.png" alt=""></span></a>
 
 </div>
-
-<div id="indexopen" >
-	<div id="imageopen">
-		<div id="regupdatesdiv">
-		
-		<form  name="regupdatesform" onsubmit="return regexcheckemail();" action="index.php" method="post">
-		<label for="emailredupdates">Please Enter your valid email address for constant updates from our party :<br></label>
-		<input type="text" name="emailregupdates" id="emailregupdates" />
-		<input type="submit" name="submitregupdates" id="submitregupdates" value="Add to our Mailling List" />
-		<form>
-		<div id="emailregupdateshelp">
-		invalid email entered
-		</div>
-		</div>
-		
-		<div id="enterwebsite" onclick='loadwebsite();'><center>Enter Website </center></div>
-	</div>
-
-</div>
-<div id="indexwebsite">
+<div id="indexwebsite" style="diaplay:block;opacity:1;">
 	<div id="indexwebsite2">
 		<div id="headerdiv">
 		header div
@@ -213,12 +123,10 @@ function mouseCoord(e)
 		</ul>
 		
 		</div>
-		<div id="newsflashdiv">
-		news
+		<div id="contentdiv">
+		
 		</div>
-		<div id="joinformdiv">
-		joinform
-		</div>
+		
 		<div id="footerdiv">
 			<div >
 				<span style="color:#FFFFCC;text-align:center;position:relative;width:99%;display:block;">AGENDA</span>
